b77d5e528bde7bdb20151f27040f10a608c14870
dima <dima.shilchikov@gmail.com>
2018-11-23 13:09:58 -0500

    Hash table - parent
    abstract hash table

---
+++ HashTable.java
@@ -0,0 +1,23 @@
+
+public abstract class  HashTable {
+	
+	private int size;
+	private int[] bucket;
+	
+	
+	public int size()
+	{
+		return size;
+	}
+	
+	public boolean isEmpty()
+	{
+		return size==0;
+	}
+	
+	public V get(int k)
+	{
+		 
+	}
+
+}
