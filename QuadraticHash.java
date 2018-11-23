0ceec39c0468750b1706d897b69330d5cacd6678
dima <dima.shilchikov@gmail.com>
2018-11-23 14:21:00 -0500

    quadratic probing hash table
    need to implement methods

---
+++ QuadraticHash.java
@@ -0,0 +1,22 @@
+
+public class QuadraticHash extends HashTable {
+
+	@Override
+	public void put(int k, Object v) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Object get(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Object remove(int k) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
