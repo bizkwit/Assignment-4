d1f79a04d9b7a08afc0022a4bdca37b4460b0b4b
victormc9 <vm06691@gmail.com>
2018-11-24 14:18:07 -0500

    Final Driver

--- driver.java
+++ driver.java
@@ -1,4 +1,6 @@
 import java.util.Random;
+
+import org.omg.CORBA.SystemException;
 public class driver {
 
 	public static void main(String[] args) {
@@ -28,7 +30,7 @@ public class driver {
 			i++;
 		}
 		
-		//MElement creation
+		//Value creation
 		i=0;
 		int[] valueHolder= new int[50];
 		while (i<50) {
@@ -38,24 +40,93 @@ public class driver {
 		}
 		
 		//Running put(k,v) 50 times per table
-		
 		i=0;
+		System.out.println("////////////////////////////	put() Validation	////////////////////////////");
 		while(i<50) {
-			System.out.println("Chaining Iteration #: "+(i+1));
+			System.out.println("Chaining Put() Iteration #: "+(i+1));
 			chainHash.put(keyHolder[i], valueHolder[i]);
 			System.out.println("\n");
 			
-			System.out.println("Linear Iteration #: "+(i+1));
+			System.out.println("Linear Put() Iteration #: "+(i+1));
 			linearHash.put(keyHolder[i], valueHolder[i]);
 			System.out.println("\n");
 
 			
-			System.out.println("Quadratic Iteration #: "+(i+1));
+			System.out.println("Quadratic Put() Iteration #: "+(i+1));
 			quadHash.put(keyHolder[i], valueHolder[i]);
 			System.out.println("\n");
 
 			i++;
 		}
+		
+		/*
+		//Running get(k,v) 50 times per table
+		i=0;
+		System.out.println("////////////////////////////	get() Validation	////////////////////////////");
+		while(i<50) {
+			System.out.println("Chaining get() Iteration #: "+(i+1));
+			System.out.println("Chain value: "+chainHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+			
+			System.out.println("Linear get() Iteration #: "+(i+1));
+			System.out.println("Linear value: "+linearHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+
+			
+			System.out.println("Quadratic get() Iteration #: "+(i+1));
+			System.out.println("Quad value: "+quadHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+
+			i++;
+		}
+		
+		*/
+		
+		/*
+		//Running remove(k) 25 times per table
+		i=0;
+		System.out.println("////////////////////////////	remove() Validation		////////////////////////////");
+		while(i<25) {
+			System.out.println("Chaining remove() Iteration #: "+(i+1));
+			System.out.println("Chain value: "+chainHash.remove(keyHolder[i]));
+			System.out.println("\n");
+			
+			System.out.println("Linear remove() Iteration #: "+(i+1));
+			System.out.println("Linear value: "+linearHash.remove(keyHolder[i]));
+			System.out.println("\n");
+
+			
+			System.out.println("Quadratic remove() Iteration #: "+(i+1));
+			System.out.println("Quad value: "+quadHash.remove(keyHolder[i]));
+			System.out.println("\n");
+
+			i++;
+		}
+		*/
+		
+		/*
+		//Running get(k,v) 50 times per table
+		i=0;
+		System.out.println("////////////////////////////	get() Validation	////////////////////////////");
+		while(i<50) {
+			System.out.println("Chaining get() Iteration #: "+(i+1));
+			System.out.println("Chain value: "+chainHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+			
+			System.out.println("Linear get() Iteration #: "+(i+1));
+			System.out.println("Linear value: "+linearHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+
+			
+			System.out.println("Quadratic get() Iteration #: "+(i+1));
+			System.out.println("Quad value: "+quadHash.get(keyHolder[i]).toString());
+			System.out.println("\n");
+
+			i++;
+		}
+		
+		*/
+		
 		
 		/////////////////
 		//	  STEP 5   //
@@ -70,10 +141,75 @@ public class driver {
 		 *									-150 values
 		 *
 		 */
+		long start=0;
+		long end=0;
+		
+		chainHash=new ChainingHash();
+		linearHash=new LinearHash(100);
+		quadHash=new QuadraticHash();
+		
+		//Key creation
+		i=0;
+		keyHolder = new int[150];
+		while (i<150) {
+			keyHolder[i]=random.nextInt(1000);
+			//System.out.println("New Key: "+keyHolder[i]+"\n");
+			i++;
+		}
+		
+		//Value creation
+		valueHolder= new int[150];
+		while (i<150) {
+			valueHolder[i]=random.nextInt(1000);
+			//System.out.println("New Value: "+valueHolder[i]);
+			i++;
+		}
+		
+		int x=50;//ITERATIONS: 50, 75, 95, 100, 150
+		
+		System.out.println("\n////////////////////////////	put() Validation	(FOR "+x+" VALUES)	 ////////////////////////////");
+		i=0;
+		start=System.nanoTime();
+		while(i<x) {
+			chainHash.put(keyHolder[i], valueHolder[i]);
+			i++;
+		}
+		end=System.nanoTime();
+		System.out.println("\n///////		Time to run put "+x+" times for Chaining Hash: "+(end-start)+"		///////\n");
+		
+		i=0;
+		start=System.nanoTime();
+		while(i<x) {
+			linearHash.put(keyHolder[i], valueHolder[i]);
+
+			i++;
+		}
+		end=System.nanoTime();
+		System.out.println("\n///////		Time to run put "+x+" times for Linear Hash: "+(end-start)+"		///////\n");
 		
 		
+		i=0;
+		start=System.nanoTime();
+		while(i<x) {
+			quadHash.put(keyHolder[i], valueHolder[i]);
+
+			i++;
+		}
+		end=System.nanoTime();
+		System.out.println("\n///////		Time to run put "+x+" times for Quad Hash: "+(end-start)+"		///////\n");
 		
-				
+		System.out.println("////////////////////////////	put() Validation	(FOR "+x+" VALUES) (Quad 101)	////////////////////////////");
+		quadHash=new QuadraticHash(101);
+		i=0;
+		start=System.nanoTime();
+		while(i<x) {
+			quadHash.put(keyHolder[i], valueHolder[i]);
+
+			i++;
+		}
+		end=System.nanoTime();
+		System.out.println("\n///////		Time to run put "+x+" times for Quad Hash: "+(end-start)+"		///////\n");
+		
 		/////////////////
 		//	  STEP 6   //
 		/////////////////
